b9eaa2e11d8fd51b4b7e04bad5f31512a46c9b6b
lshimizu0 <lupshimizu@gmail.com>
2026-03-16 12:45:36 -0600

    ex1 Partial submission

---
+++ ex1.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.nextLeft = None
+        self.nextRight = None
+
+def insertNode(headNode, insertedNode):
+    searchNode = headNode
+    while True:
+        if(searchNode.data>insertedNode.data and searchNode.nextRight is not None):
+            searchNode = searchNode.nextRight
+        elif(searchNode.data>insertedNode.data and searchNode.nextRight is None):
+            searchNode.nextRight = insertedNode
+            break
+        elif(searchNode.data<insertedNode.data and searchNode.nextLeft is not None):
+            searchNode = searchNode.nextLeft
+        elif(searchNode.data<insertedNode.data and searchNode.nextLeft is None):
+            searchNode.nextLeft = insertedNode
+            break
+        
+def traverseAndPrint(head):
+  currentNode = head
+  while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+  print("null")
